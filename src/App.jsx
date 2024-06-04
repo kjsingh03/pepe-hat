caff519946426624d96fb5f82a4ecf7277fd02c2
Karanjot Singh <k.jsingh.12003@gmail.com>
2024-06-04 19:57:47 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -51,27 +51,27 @@ const App = () => {
     const img = imageRef.current;
     const hatImg = new Image();
     hatImg.src = hatImage;
-  
+
     // Wait for both the main image and hat image to load
     await Promise.all([
       new Promise((resolve) => (img.complete ? resolve() : (img.onload = resolve))),
       new Promise((resolve) => (hatImg.complete ? resolve() : (hatImg.onload = resolve))),
     ]);
-  
+
     // Create a new canvas with higher dimensions
     const canvas = document.createElement('canvas');
     const context = canvas.getContext('2d');
     canvas.width = img.width; // Increase canvas width (example: double the width)
     canvas.height = img.height; // Increase canvas height (example: double the height)
     context.drawImage(img, 0, 0, canvas.width, canvas.height);
-  
+
     const { x, y, width, height, rotate } = hat;
     context.save();
     context.translate(x + width / 2, y + height / 2);
     context.rotate((rotate * Math.PI) / 180);
     context.drawImage(hatImg, -width / 2, -height / 2, width, height);
     context.restore();
-  
+
     // Convert canvas to Blob and download
     canvas.toBlob((blob) => {
       const link = document.createElement('a');
@@ -83,13 +83,13 @@ const App = () => {
       URL.revokeObjectURL(link.href); // Clean up the URL.createObjectURL reference
     }, 'image/png');
   };
-  
-  
+
+
 
   return (
     <>
-      <div className="h-[100vh] overflow-y-auto">
-        <div className='h-screen z-40 bg-[url(./assets/bg1.webp)] bg-no-repeat bg-center bg-cover bg-fixed flex items-center justify-center'>
+      <div className="">
+        <div className='h-screen z-40 bg-[url(./assets/bg1.webp)] bg-no-repeat bg-[-37rem_0rem] xs:bg-[-27rem_0rem] sm:bg-center bg-cover bg-fixed flex items-center justify-center'>
 
           <div className="flex flex-col backdrop-blur-[5px h-[35rem] text-white rounded-lg w-[35rem] justify-center items-center gap-6">
             <p className="text-center text-2xl font-semibold">Upload an image to add pepe hat</p>
@@ -100,7 +100,7 @@ const App = () => {
               image &&
               <>
                 <div ref={screenRef} className='screen z-50 w-screen h-screen fixed left-0 top-0 flex flex-col gap-4 items-center justify-center backdrop-blur-sm'>
-                  <div className="modal flex flex-col gap-4 items-center justify-center mx-auto p-5 w-full xs:w-[70vw] sm:w-[50vw] 2xl:w-[40vw] h-[20rem]">
+                  <div className="modal border-slate-400/20 rounded-lg border flex flex-col gap-4 items-center justify-center mx-auto p-5 w-full xs:w-[70vw] md:w-[50vw] 2xl:w-[40vw] h-[20rem]">
                     <div className="h-auto w-auto max-h-[13.5rem] max-w-[24rem] overflow-hidden" style={{ position: 'relative', display: 'inline-block' }}>
                       <img ref={imageRef} src={image} alt="Uploaded" crossOrigin="anonymous" className='w-full h-full object-cover' />
                       <canvas ref={canvasRef} style={{ display: 'none' }} />
@@ -168,6 +168,14 @@ const App = () => {
                 </div>
               </>
             }
+
+          </div>
+        </div>
+        <div className='h-screen z-40 bg-[url(./assets/bg2.jpg)] bg-no-repeat bg-center bg-cover bg-fixed flex items-center justify-center'>
+
+          <div className="flex flex-col backdrop-blur-[5px h-[35rem] text-white rounded-lg w-[35rem] justify-center items-center gap-6">
+            <p className="text-center text-2xl font-semibold">Upload an image to add pepe hat</p>
+            <div onClick={() => { window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });console.log('click') }} className='text-white cursor-pointer px-5 rounded-3xl py-3 bg-[#32A430] font-semibold'>Upload an image</div>
 
           </div>
         </div>
